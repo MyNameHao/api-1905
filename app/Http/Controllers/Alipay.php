2ec96a91d28d20976c431455f6cfac503c64c88e
sunhao <1154503210@qq.com>
2020-01-02 04:52:59 +0000

    alipay

--- app/Http/Controllers/Alipay.php
+++ app/Http/Controllers/Alipay.php
@@ -25,8 +25,8 @@ class Alipay extends Controller
             'sign_type'=>$config['sign_type'],
             'timestamp'=>$timestamp,
             'version'=>'1.0',
-//            'notify_url'=>'http://sh.lizhijun.fun/alipay/notify',
-            'return_url '=>'http://sh.lizhijun.fun/alipay/return',
+            'notify_url'=>'http://sh.lizhijun.fun/alipay/notify',
+            'return_url'=>'http://sh.lizhijun.fun/alipay/return',
             'biz_content'=>json_encode($biz_content),
         ];
 
